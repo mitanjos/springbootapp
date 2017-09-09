4a2648dc73f53c66783605489777fce540666c26
Keyur joshi <keyurjoshi@Keyurs-MacBook-Air.local>
2017-09-09 23:15:54 +0530

    POST medhod added

--- src/main/java/com/practice/awsapp/controller/NAVController.java
+++ src/main/java/com/practice/awsapp/controller/NAVController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,11 @@ public class NAVController {
 	@RequestMapping(path="/{amfiId}",method=RequestMethod.GET)
 	public NAVBean findByAMFIId(@PathVariable("amfiId")String amfiId) {
 		return repo.findByAmfiId(amfiId);
+	}
+	
+	@RequestMapping(path="/", method=RequestMethod.POST)
+	public NAVBean createEntry(@RequestBody NAVBean newNav) {
+		repo.save(newNav);
+		return newNav;
 	}
 }
