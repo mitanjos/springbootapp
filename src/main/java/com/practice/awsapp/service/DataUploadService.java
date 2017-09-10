a6ff7f4c0f5981881313ab0d704b9c9246843393
Keyur joshi <keyurjoshi@Keyurs-MacBook-Air.local>
2017-09-11 00:29:22 +0530

    For batch job

--- src/main/java/com/practice/awsapp/service/DataUploadService.java
+++ src/main/java/com/practice/awsapp/service/DataUploadService.java
@@ -19,40 +19,46 @@ import com.practice.awsapp.repo.NAVRepo;
 
 @Component
 public class DataUploadService {
-	
+
 	@Autowired
 	NAVRepo repo;
-	
+
 	@Value("${navdata.file.location}")
 	private String datafilelocation;
-	
-	@Scheduled(cron="0 0 10 * * ?")
+
+	@Scheduled(cron = "0 0 10 * * ?")
 	public void loadData() {
-		System.out.println("Loading data @ "+new Date());
+		System.out.println("Loading data @ " + new Date());
+		if(datafilelocation==null || "".equals(datafilelocation.trim())) {
+			datafilelocation = "";
+		}
 		File dataDir = new File(datafilelocation);
-		if(dataDir.isDirectory()) {
+		if (dataDir.isDirectory()) {
 			File[] fileList = dataDir.listFiles();
-			for(File file:fileList) {
+			for (File file : fileList) {
 				processFile(file);
 			}
 		}
 	}
+
 	private void processFile(File file) {
 		BufferedReader reader = null;
 		try {
-			reader = new BufferedReader(new InputStreamReader(
-					new FileInputStream(file)));
+			reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
 			String line = null;
 			int i = 0;
 			while ((line = reader.readLine()) != null) {
 				if (i != 0) {
 					if (line.lastIndexOf(';') > 0) {
 						NAVBean bean = buildRecordFromString(line);
-						repo.save(bean);
+						if (bean != null) {
+							repo.save(bean);
+						}
 					}
 				}
 				i++;
 			}
+			file.delete();
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -65,6 +71,7 @@ public class DataUploadService {
 			}
 		}
 	}
+
 	private static NAVBean buildRecordFromString(String str) {
 		String[] values = str.split(";");
 		System.out.println(values.length);
@@ -74,9 +81,9 @@ public class DataUploadService {
 			bean.setPayoutISIN(values[1]);
 			bean.setReInvISIN(values[2]);
 			bean.setName(values[3]);
-			bean.setNavVal(NumberUtils.isNumber(values[4])?Double.valueOf(values[4]):0d);
-			bean.setRepurchasePrice(NumberUtils.isNumber(values[5])?Double.valueOf(values[5]):0d);
-			bean.setSalePrice(NumberUtils.isNumber(values[6])?Double.valueOf(values[6]):0d);
+			bean.setNavVal(NumberUtils.isNumber(values[4]) ? Double.valueOf(values[4]) : 0d);
+			bean.setRepurchasePrice(NumberUtils.isNumber(values[5]) ? Double.valueOf(values[5]) : 0d);
+			bean.setSalePrice(NumberUtils.isNumber(values[6]) ? Double.valueOf(values[6]) : 0d);
 			bean.setPriceDate(values[7]);
 			return bean;
 		}
