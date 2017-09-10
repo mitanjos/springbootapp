ba2dfedae46174d3f23e47178df191703764644a
Keyur joshi <keyurjoshi@Keyurs-MacBook-Air.local>
2017-09-10 12:45:11 +0530

    NAVBean updated

--- src/main/java/com/practice/awsapp/bean/NAVBean.java
+++ src/main/java/com/practice/awsapp/bean/NAVBean.java
@@ -1,5 +1,7 @@
 package com.practice.awsapp.bean;
 
+import java.util.Date;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -9,22 +11,73 @@ public class NAVBean {
 	private String id;
 	
 	private String amfiId;
-
+	
+	private String payoutISIN;
+	private String reInvISIN;
+	private String name;
+	private Double navVal;
+	private Double repurchasePrice;
+	private Double salePrice;
+	private String priceDate;
+	private Date date;
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getAmfiId() {
 		return amfiId;
 	}
-
 	public void setAmfiId(String amfiId) {
 		this.amfiId = amfiId;
 	}
-	
-	
+	public String getPayoutISIN() {
+		return payoutISIN;
+	}
+	public void setPayoutISIN(String payoutISIN) {
+		this.payoutISIN = payoutISIN;
+	}
+	public String getReInvISIN() {
+		return reInvISIN;
+	}
+	public void setReInvISIN(String reInvISIN) {
+		this.reInvISIN = reInvISIN;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getNavVal() {
+		return navVal;
+	}
+	public void setNavVal(Double navVal) {
+		this.navVal = navVal;
+	}
+	public Double getRepurchasePrice() {
+		return repurchasePrice;
+	}
+	public void setRepurchasePrice(Double repurchasePrice) {
+		this.repurchasePrice = repurchasePrice;
+	}
+	public Double getSalePrice() {
+		return salePrice;
+	}
+	public void setSalePrice(Double salePrice) {
+		this.salePrice = salePrice;
+	}
+	public String getPriceDate() {
+		return priceDate;
+	}
+	public void setPriceDate(String priceDate) {
+		this.priceDate = priceDate;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
 }
