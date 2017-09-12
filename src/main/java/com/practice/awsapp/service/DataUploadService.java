3cec19d282aa0a0e4c44a7591e9ff1485658f763
Keyur joshi <keyurjoshi@Keyurs-MacBook-Air.local>
2017-09-12 21:53:05 +0530

    sced

--- src/main/java/com/practice/awsapp/service/DataUploadService.java
+++ src/main/java/com/practice/awsapp/service/DataUploadService.java
@@ -30,7 +30,7 @@ public class DataUploadService {
 	@Autowired
 	NAVRepo repo;
 
-	@Scheduled(cron = "*/5 * * * * ?")
+	@Scheduled(cron = "* */10 * * * ?")
 	public void loadData() {
 		System.out.println("Loading data @ " + new Date());
 		if(datafilelocation==null || "".equals(datafilelocation.trim())) {
